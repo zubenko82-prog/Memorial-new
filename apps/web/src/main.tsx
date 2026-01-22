1da9536aa733082737af376c74563c8a1556fc8b
unknown <zubenko82@gmail.com>
2026-01-22 08:16:25 +0300

    chore(bot): switch to JS and fix import in webhook

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./styles/fonts.css";
 import { tgSafeInit } from "./lib/tg";
 tgSafeInit();
+import App from "./App";
+ReactDOM.createRoot(...).render(<App />);
+
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
