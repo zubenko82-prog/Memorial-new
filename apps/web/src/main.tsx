afa5a7e922f3107b2b3234c1ca6b3136429941aa
unknown <zubenko82@gmail.com>
2026-01-22 08:17:38 +0300

    chore(bot): switch to JS and fix import in webhook

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/fonts.css";
 import { tgSafeInit } from "./lib/tg";
-tgSafeInit();
 import App from "./App";
 ReactDOM.createRoot(...).render(<App />);
 
-
+tgSafeInit();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <div style={{ padding: 16, color: "#fff", background: "#111", minHeight: "100vh" }}>
